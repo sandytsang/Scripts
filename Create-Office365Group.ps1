409f30c214f2b24c1acf7216c394ffa7e2e4452c
sandytsang <28437644+sandytsang@users.noreply.github.com>
2021-06-07 15:23:01 +0300

    updated

--- Create-Office365Group.ps1
+++ Create-Office365Group.ps1
@@ -11,12 +11,12 @@ $check = Get-UnifiedGroup -Identity $GroupName -ErrorAction SilentlyContinue
 
 If (!$check) {
     Write-Host "Creating $GroupName"
-    
+
     try {
         #Create New O365 Group
-        New-UnifiedGroup -DisplayName $GroupName -ErrorAction Stop
+        New-UnifiedGroup -DisplayName $GroupName
         Start-Sleep 3
-    
+
         #Configure Group settings
         Write-Host "Configuring group settings"
         Set-UnifiedGroup -Identity $GroupName -UnifiedGroupWelcomeMessageEnabled:$false -RequireSenderAuthenticationEnabled:$true -AccessType "Private" -AutoSubscribeNewMembers -SubscriptionEnabled:$true -Verbose -ErrorAction Stop
